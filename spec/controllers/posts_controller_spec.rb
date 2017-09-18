340ff38d374200a5fdbf0fe136fd6b5bbebb14d5
Simon <Simon@Simons-MacBook-Air.local>
2017-09-17 20:06:52 -0700

    CRUD checkpoint completed

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,24 +3,33 @@ require 'rails_helper'
 RSpec.describe PostsController, type: :controller do
   let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
+describe "GET #index" do
+  it "returns http success" do
+    get :index
+    expect(response).to have_http_status(:success)
   end
+end
 
-  it "assigns [my_post] to @posts" do
-     get :index
-     expect(assigns(:posts)).to eq([my_post])
+it "assigns [my_post] to @posts" do
+   get :index
+   expect(assigns(:posts)).to eq([my_post])
+ end
+
+ describe "GET show" do
+   it "returns http success" do
+     get :show, params: { id: my_post.id }
+     expect(response).to have_http_status(:success)
+   end
+   it "renders the #show view" do
+     get :show, params: { id: my_post.id }
+     expect(response).to render_template :show
    end
 
-   #  describe "GET show" do
- #    it "returns http success" do
- #      get :show
- #      expect(response).to have_http_status(:success)
- #    end
- #  end
+   it "assigns my_post to @post" do
+     get :show, params: { id: my_post.id }
+     expect(assigns(:post)).to eq(my_post)
+   end
+ end
 
  describe "GET new" do
    it "returns http success" do

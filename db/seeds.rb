32926ce5d8ac532bf40c5b8f3bc21dc479d8efa9
Simon <Simon@Simons-MacBook-Air.local>
2017-09-18 21:33:17 -0700

    More CRUD assignment completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,15 @@ posts = Post.all
   )
 end
 
-Post.find_or_create_by!(title: "A unique title", body: "A unique body")
+100.times do
+  Question.create!(
+  title: RandomData.random_sentence,
+  body: RandomData.random_paragraph,
+  resolved: false
+  )
+end
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"

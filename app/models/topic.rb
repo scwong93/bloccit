cc06779acb3a2641eed925adaa946034bf826fdb
Simon <Simon@Simons-MacBook-Air.local>
2017-09-25 13:49:19 -0700

    Validation assignments completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,7 @@
 class Topic < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :sponsoredpost
+
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 15 }, presence: true
 end
